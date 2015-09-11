8610e264f380c538eb1265ef4057e0745b1c405d
Adam <leodafinchy@gmail.com>
2015-09-11 16:58:07 +0100

    Added LineSegment, Intersect, and Matrixes to library

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 exports.Vector2 = require('./src/Vector2').Vector2;
+exports.Intersect2 = require('./src/Intersect2').Intersect2;
+exports.Matrix2 = require('./src/Matrix2').Matrix2;
+exports.Matrix3 = require('./src/Matrix3').Matrix3;
+exports.LineSegment2 = require('./src/LineSegment2').LineSegment2;
